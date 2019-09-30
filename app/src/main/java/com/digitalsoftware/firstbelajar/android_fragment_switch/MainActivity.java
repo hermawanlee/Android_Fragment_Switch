4cb06c03ac5a00436ea281e6a0b723e20f53d5d7
hermawanlee <51147195+hermawanlee@users.noreply.github.com>
2019-09-30 20:26:59 +0700

    Initial commit

---
+++ app/src/main/java/com/digitalsoftware/firstbelajar/android_fragment_switch/MainActivity.java
@@ -0,0 +1,17 @@
+package com.digitalsoftware.firstbelajar.android_fragment_switch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bt_satu,bt_dua, bt_tiga;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
